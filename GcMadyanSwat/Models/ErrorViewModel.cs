7604bb20327c1eb8fa5b6164b51f7173b49d87aa
KHAAN's <KHAAN's@DESKTOP-R7EJSCH>
2024-06-22 23:58:15 +0500

    Add project files.

---
+++ GcMadyanSwat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GcMadyanSwat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
